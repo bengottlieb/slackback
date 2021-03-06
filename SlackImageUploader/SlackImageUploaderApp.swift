cef2341370b2d69c9b1529cfeb43d57174d7b7aa
Ben Gottlieb <ben@standalone.com>
2021-03-06 05:48:52 -0600

    Initial Commit

---
+++ SlackImageUploader/SlackImageUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackImageUploaderApp.swift
+//  SlackImageUploader
+//
+//  Created by Ben Gottlieb on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SlackImageUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
